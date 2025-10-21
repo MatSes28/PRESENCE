3bfb14e8aa2e70043bc9ea196c4da9cee66a724c
mattferia <matt.feria@clsu2.edu.ph>
2025-10-21 09:27:51 +0800

    1

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/ws': {
-        target: 'ws://localhost:3000',
+      "/api": "http://localhost:3000",
+      "/ws": {
+        target: "ws://localhost:3000",
         ws: true,
       },
     },
   },
-})
+});
