e63d2e51b6b50764eb21f499e382d6caa8f85edf
mattferia <matt.feria@clsu2.edu.ph>
2025-10-22 22:49:38 +0800

    5

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -4,9 +4,7 @@ export default defineConfig({
   schema: "./src/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
-  driver: "pg",
   dbCredentials: {
-    connectionString:
-      "postgresql://postgres:ivXwpKRBFPqDEzhjzMlfQOpBXZorhyTy@mainline.proxy.rlwy.net:22250/railway",
+    url: "postgresql://postgres:ivXwpKRBFPqDEzhjzMlfQOpBXZorhyTy@mainline.proxy.rlwy.net:22250/railway",
   },
 });
