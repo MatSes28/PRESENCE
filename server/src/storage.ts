901e810b478e1e9369b109e462e9d79394197f01
mattferia <matt.feria@clsu2.edu.ph>
2025-10-22 22:16:36 +0800

    4

--- server/src/storage.ts
+++ server/src/storage.ts
@@ -1,18 +1,20 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import * as schema from './schema.js'
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema.js";
 
-const connectionString = process.env.DATABASE_URL!
+const connectionString =
+  process.env.DATABASE_URL ||
+  "postgresql://postgres:ivXwpKRBFPqDEzhjzMlfQOpBXZorhyTy@mainline.proxy.rlwy.net:22250/railway";
 
 if (!connectionString) {
-  throw new Error('DATABASE_URL environment variable is required')
+  throw new Error("DATABASE_URL environment variable is required");
 }
 
 // Create the connection
-const client = postgres(connectionString, { prepare: false })
+const client = postgres(connectionString, { prepare: false });
 
 // Create the database instance
-export const db = drizzle(client, { schema })
+export const db = drizzle(client, { schema });
 
 // Export types
-export type Database = typeof db
+export type Database = typeof db;
