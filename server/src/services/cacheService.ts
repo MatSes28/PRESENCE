537e61fad11ed4d72ecd18ec23c024c43964f27b
MatSes28 <mattferia777@gmail.com>
2026-01-06 13:07:20 +0800

    Revert "try 2.9"
    This reverts commit f67fc418fb9301a4f129e7c749e1c1d71e266a7c.

--- server/src/services/cacheService.ts
+++ server/src/services/cacheService.ts
@@ -60,7 +60,6 @@ class CacheService {
   }
 
   private async connect(): Promise<void> {
-    if (this.isConnected) return;
     try {
       await this.client.connect();
       console.log("Connected to Redis successfully");
